3df7d7feb1a4ae164634e82e6841bfdcca8bab3f
vaarigupta <vaarigupta24@gmail.com>
2018-07-14 02:49:18 +0530

    max sum 2sub array added

--- ARRAYS/MAXSUM_SUB_ARRAY2.cpp
+++ ARRAYS/MAXSUM_SUB_ARRAY2.cpp
@@ -1,1 +1,46 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,a[100],maxsum=0,left =0,right=0,Cumsum[100] ={0};
+    cin>>n;
+    cin>>a[0];
+    Cumsum[0] = a[0];
+    for(int i =1;i<n;i++)
+    {
+        cin>>a[i];
+        Cumsum[i] = Cumsum[i-1] + a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+       for(int j =i;j<n;j++)
+       {
+           int currentsum = 0;
+           if(i>0)
+           {
+              currentsum = Cumsum[j] - Cumsum[i-1];
+           }
+           else
+           {
+              currentsum = Cumsum[j];
+           }
+
+           if(currentsum > maxsum)
+           {
+               maxsum = currentsum;
+               left = i ;
+               right = j;
+           }
+       }
+    }
+    cout<<maxsum<<endl;
+    for(int i = left;i<=right;i++) /// subarray displayed whose sum is max
+    {
+        cout<<a[i]<<" ,";
+    }
+
+
+    return 0;
+}
+
 
