d7ff50dd98978eb25f875fd8dcb7df4dc3aa3005
vaarigupta <vaarigupta24@gmail.com>
2018-07-14 02:15:09 +0530

    max sum 1sub array added

--- ARRAYS/MAXSUM_SUB_ARRAY1.cpp
+++ ARRAYS/MAXSUM_SUB_ARRAY1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int n,a[100],maxsum=0;
+    int n,a[100],maxsum=0,left =0,right=0;
     cin>>n;
     for(int i =0;i<n;i++)
     {
@@ -17,15 +17,19 @@ int main()
 
                currentsum += a[k];
            }
-           if(currentsum>maxsum)
+           if(currentsum > maxsum)
            {
                maxsum = currentsum;
+               left = i ;
+               right = j;
            }
-
-
        }
     }
     cout<<maxsum<<endl;
+    for(int i = left;i<=right;i++) /// subarray displayed whose sum is max
+    {
+        cout<<a[i]<<" ,";
+    }
 
 
     return 0;
