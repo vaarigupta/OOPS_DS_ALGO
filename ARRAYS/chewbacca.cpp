c3e78ce501364bb86719996cd92b798a72ccf39e
vaarigupta <vaarigupta24@gmail.com>
2018-07-15 13:02:04 +0530

    rotate image

--- ARRAYS/chewbacca.cpp
+++ ARRAYS/chewbacca.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 int main()
 {
-   int n ,rem,i=0;
-   int *ptr = new int[100];
-   cin>>n;
-   while(n>0)
-   {
-     rem = n%10;
-     ptr[i] = rem ;
-     n = n/10;
-     i++;
-   }
-   int s = sizeof(ptr)/sizeof(int);
-   cout<<s;
+//   int n ,rem,i=0;
+//   int *ptr = new int[100];
+//   cin>>n;
+//   while(n>0)
+//   {
+//     rem = n%10;
+//     ptr[i] = rem ;
+//     n = n/10;
+//     i++;
+//   }
+//   int s = sizeof(ptr)/sizeof(int);
+//   cout<<s;
+
+
 
 }
