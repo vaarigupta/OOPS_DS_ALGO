badfcadb2c76e62137f5835ff3a505c3589ba0e0
vaarigupta <vaarigupta24@gmail.com>
2018-12-28 20:32:44 +0530

    Linked List Merge func done

--- LINKED_LIST_OPERATIONS/SortingLL.cpp
+++ LINKED_LIST_OPERATIONS/SortingLL.cpp
@@ -53,6 +53,77 @@ void print (node *head)
     }
     cout<<endl;
 }
+node* &mergeLL(node *&head1 , node *&head2)
+{
+    node* temp1 = head1;
+    node* temp2 = head2;
+    node* mergeTemp = NULL;
+    while(temp1!= NULL && temp2!=NULL)
+    {
+        if(temp1->data <= temp2->data)
+        {
+
+            addAtFront(mergeTemp , temp1->data);
+            temp1= temp1->next;
+
+        }
+        else
+        {
+            addAtFront(mergeTemp , temp2->data);
+            temp2 = temp2->next;
+
+        }
+
+
+    }
+    if(temp1!=NULL)
+    {
+        while(temp1!= NULL)
+    {
+        addAtFront(mergeTemp,temp1->data);
+        temp1 = temp1->next;
+    }
+
+    }
+
+      if(temp2!= NULL)
+      {
+             while(temp2!= NULL)
+    {
+        addAtFront(mergeTemp,temp2->data);
+        temp2 = temp2->next;
+    }
+      }
+
+
+    return mergeTemp;
+}
+node* merge(node * head1 , node* head2)
+{
+    node* c = NULL;
+    node* a = head1;
+    node* b = head2;
+    if(a== NULL)
+    {
+        return b;
+    }
+    if(b == NULL)
+    {
+        return a;
+    }
+    if(a->data <= b->data)
+    {
+       c = a;
+       c->next = merge(a->next , b);
+    }
+    else
+    {
+    c = b;
+    c->next = merge(a , b->next);
+
+    }
+    return c;
+}
 istream& operator>>(istream &is , node *&head)
 {
     takeInput(head);
@@ -65,9 +136,15 @@ ostream& operator<<(ostream &os , node * head)
 }
 int main()
 {
-    node * head = NULL;
-    cin>>head;
-    cout<<head;
+    node * head1 = NULL;
+    node * head2 = NULL;
+    cin>>head1;
+    cout<<head1;
+    cin>>head2;
+    cout<<head2;
+    node *MergeList = merge(head1 , head2);
+    cout<<MergeList;
+
 
 
     return 0;
