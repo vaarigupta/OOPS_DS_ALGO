a200f006c9f9e5ad174ef390ca1643c6bfac7081
vaarigupta <vaarigupta24@gmail.com>
2019-01-02 23:05:45 +0530

    Queue header file added

---
+++ QUEUES/Queue.h
@@ -0,0 +1,42 @@
+using namespace std;
+class Queue
+{
+    int q*;
+    int f , r , maxS , currS ;
+
+public:
+    Queue(int d)
+    {
+       q = new int[d];
+       maxS = d;
+       currS = 0;
+       f =0;
+       r =  maxS -1;
+    }
+    bool isEmpty()
+    {
+        return currS==0;
+    }
+    bool isFull()
+    {
+        return currS == maxS;
+    }
+    void push(int d)
+    {
+        if(!isFull())
+        {
+            r = (r+1)%maxS;
+            q[r] = d;
+            currS++;
+        }
+
+    }
+    void pop()
+    {
+        if(!isEmpty())
+        {
+            f = (f+1)%maxS;
+            currS--;
+        }
+    }
+};
