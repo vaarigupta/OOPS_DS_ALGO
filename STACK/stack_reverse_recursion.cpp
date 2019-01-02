e8d65b08e870b48de47e4fdd2fca1907120c8d29
vaarigupta <vaarigupta24@gmail.com>
2019-01-02 16:23:51 +0530

    stack reverse using recursion description

--- STACK/stack_reverse_recursion.cpp
+++ STACK/stack_reverse_recursion.cpp
@@ -11,6 +11,9 @@ void print(stack <int> s)
     }
     cout<<endl;
 }
+/// If stack is empty then we push the top element of the function call stack into the original stack
+///if its not empty then we pop all the elements until its empty and again push the topmost element of function call
+///stack into the original stack and push again the held elements into the original stack
 void insertAtbottom(stack<int> &s , int item)
 {
     if(s.empty())
@@ -27,6 +30,9 @@ void insertAtbottom(stack<int> &s , int item)
 
 
 }
+/// we pop the elements of the stack until its empty and hold all the elements onto the function call stack
+///when the stack becomes empty then we call insertAtBottom function which insert all the held items
+///one by one at the bottom of the stack
 void reverseRec(stack <int> &s)
 {
     if(s.empty())
