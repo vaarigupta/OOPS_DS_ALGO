628e4587708930c42fd31474cedc5d0f23ee555e
vaarigupta <vaarigupta24@gmail.com>
2019-01-06 03:13:38 +0530

    Binary Tree with Postorder traversal

--- BINARY_TREE/PostOrderTreeTraversal.cpp
+++ BINARY_TREE/PostOrderTreeTraversal.cpp
@@ -1,1 +1,54 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node* left;
+    node * right;
 
+    node(int d)
+    {
+
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+node* buildTree()
+{
+    int d;
+    cout<<"Enter the Data ";
+    cin>>d;
+    ///Base Case
+    if(d == -1)
+    {
+        return NULL;
+    }
+
+    ///rec Case
+    node * n = new node(d);
+    n->left = buildTree();
+    n->right = buildTree();
+    return n;
+
+}
+void PostOrderTraversal(node* root)
+{
+    ///Base Case
+    if(root == NULL)
+    {
+        return ;
+    }
+    ///Rec Case
+    PostOrderTraversal(root->left);
+    PostOrderTraversal(root->right);
+    cout<<root->data<<" ";
+}
+int main()
+{
+    node * root = buildTree();
+    PostOrderTraversal(root);
+
+    return 0;
+}
