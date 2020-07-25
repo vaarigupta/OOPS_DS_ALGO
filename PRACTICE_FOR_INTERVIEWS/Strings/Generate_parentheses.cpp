c33444259b27f96b4f194ccd453c3461575b3d64
vaari gupta <vaarigupta24@gmail.com>
2020-07-26 04:29:15 +0530

    strings ques

--- PRACTICE_FOR_INTERVIEWS/Strings/Generate_parentheses.cpp
+++ PRACTICE_FOR_INTERVIEWS/Strings/Generate_parentheses.cpp
@@ -1,1 +1,56 @@
 
+/*Generate Parentheses
+https://leetcode.com/problems/generate-parentheses/
+*/
+#include<iostream>
+#include<cstring>
+#include<vector>
+using namespace std;
+void generateParenthesis(int n,int open,int close,string output, vector<string> &combinations)
+{
+	if(output.size()==n*2)
+	{
+		combinations.push_back(output);
+		return;
+	}
+	if(open<n)
+	{
+		generateParenthesis(n,open+1,close,output + '(', combinations);
+
+	}
+	if(open> close)
+	{
+		generateParenthesis(n,open,close+1,output + ')', combinations);
+
+	}
+
+}
+int main()
+{
+int n;
+cin>>n;
+vector<string> combinations;
+if(n==0)
+{
+	cout<<"NO COMBINATIONS"<<endl;
+}
+else
+{
+	generateParenthesis(n,0,0,"", combinations);
+	for(int i=0;i<combinations.size();i++)
+	{
+		cout<<combinations[i]<<endl;
+	}
+
+}
+	return 0;
+}
+/*
+3
+((()))
+(()())
+(())()
+()(())
+()()()
+
+*/
